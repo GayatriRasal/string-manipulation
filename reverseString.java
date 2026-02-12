dfa0385936c23c92f8028cebdda92731f2fc0112
Ritika Nemade <157018698+RitikaNemade@users.noreply.github.com>
2026-02-12 22:37:30 +0530

    added reverse String code

---
+++ reverseString.java
@@ -0,0 +1,8 @@
+public class reverseString {
+
+    public static String reverse(String input) {
+        StringBuilder sb = new StringBuilder(input);
+        return sb.reverse().toString();
+    }
+
+}
