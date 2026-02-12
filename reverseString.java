3b543830b89ca40e87ad3cdad15375b0d986111d
Ritika Nemade <157018698+RitikaNemade@users.noreply.github.com>
2026-02-12 22:53:17 +0530

    updated reverse string code

--- reverseString.java
+++ reverseString.java
@@ -1,8 +1,22 @@
 public class reverseString {
 
     public static String reverse(String input) {
-        StringBuilder sb = new StringBuilder(input);
-        return sb.reverse().toString();
+        
+        char[] arr = input.toCharArray();
+        int start = 0;
+        int end = arr.length - 1;
+
+        // Swap characters
+        while (start < end) {
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        return new String(arr);
     }
 
 }
