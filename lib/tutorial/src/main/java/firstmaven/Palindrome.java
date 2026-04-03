320200fc1c1ead3b28f237b755737bbcdca1794d
Samruddhi90 <salunkhesamruddhi07@gmail.com>
2026-04-03 11:08:36 +0530

    maven

---
+++ lib/tutorial/src/main/java/firstmaven/Palindrome.java
@@ -0,0 +1,41 @@
+package firstmaven;
+
+/**
+ * Hello world!
+ *
+ */
+// public class App 
+// {
+//     public static void main( String[] args )
+//     {
+//         System.out.println( "Hello World!" );
+//     }
+// }
+
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String input) {
+
+        // Handle null safely
+        if (input == null) {
+            return false;
+        }
+
+        // Remove spaces and convert to lowercase
+        String cleaned = input.replaceAll("\\s+", "").toLowerCase();
+
+        int left = 0;
+        int right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
